f02cd71069717f2d68142ba36c69a4b4b11f3306
mmdcloud <mmd26.aka@gmail.com>
2024-08-25 07:11:43 +0530

    update:terraform file path changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "nv-resources" {
   region_name = "nv"
   cidr        = "10.1.0.0/24"
   subnets     = var.nv_subnets
-  user_data   = filebase64("${path.module}/nv_user_data.sh")
+  user_data   = filebase64("${path.module}/../scripts/nv_user_data.sh")
   providers = {
     aws = aws.nv
   }
@@ -15,7 +15,7 @@ module "mumbai-resources" {
   azs         = var.mumbai_azs
   region_name = "mumbai"
   cidr        = "10.2.0.0/24"
-  user_data   = filebase64("${path.module}/mumbai_user_data.sh")
+  user_data   = filebase64("${path.module}/../scripts/mumbai_user_data.sh")
   subnets     = var.mumbai_subnets
   providers = {
     aws = aws.mumbai
